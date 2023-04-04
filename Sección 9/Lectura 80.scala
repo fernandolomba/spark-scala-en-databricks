9a306a4a2a2ecf937a2a5d3f1ff9f825ee528883
Fer <98801855+fernandolomba@users.noreply.github.com>
2023-04-04 23:59:31 +0200

    Update Lectura 80.scala

--- Sección 9/Lectura 80.scala
+++ Sección 9/Lectura 80.scala
@@ -7,10 +7,10 @@ display(vuelos)
 import org.apache.spark.sql.functions.col
 
 val nuevoDF = vuelos.filter(col("MONTH")isin(6, 7, 8))
-										.withColumn("distancia_tiempo_aire", col("DISTANCE") / col("AIR_TIME"))
-										.select(
-										  col("AIR_TIME"),
-										  col("distancia_tiempo_aire")
-										).where(col("AIRLINE").isin("AA", "DL", "AS"))
+	.withColumn("distancia_tiempo_aire", col("DISTANCE") / col("AIR_TIME"))
+	.select(
+		col("AIR_TIME"),
+		col("distancia_tiempo_aire")
+	).where(col("AIRLINE").isin("AA", "DL", "AS"))
 
 nuevoDF.explain(true)
